5ded43e01064f597bce3706a617bb068d9f5a26e
curtis <curtis@serverascode.com>
2024-12-03 07:04:37 -0500

    version 0.0.24

--- docker/scripts/install-drifted.sh
+++ docker/scripts/install-drifted.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Download and install the drifted binary
-OWNER="ccollicutt"
-REPO="c-drifted-binary"
-VERSION=$(cat ../../VERSION)
-wget -O /usr/local/bin/drifted https://github.com/${OWNER}/${REPO}/releases/download/v${VERSION}/drifted
+wget -O /usr/local/bin/drifted ${DOWNLOAD_URL}
+
+# Check if download was successful
+if [ ! -f /usr/local/bin/drifted ]; then
+  echo "Failed to download drifted binary!"
+  exit 1
+fi
+
 chmod +x /usr/local/bin/drifted
 
 # Execute the binary
